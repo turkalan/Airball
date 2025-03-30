30613058d0999bb810a18b211749a0b770c14b06
turkalan <alan.turk57@gmail.com>
2025-03-30 23:59:28 +0200

    Commentary

--- common/compose/src/main/java/com/kobayagi/airball/common/compose/extensions/Activity.kt
+++ common/compose/src/main/java/com/kobayagi/airball/common/compose/extensions/Activity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
 
-// Way to find an activity
 fun Context.findActivity(): Activity? = when (this) {
     is Activity -> this
     is ContextWrapper -> baseContext.findActivity()
